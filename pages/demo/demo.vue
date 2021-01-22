87a83cd25273395ac0ae88a9b2ae44652f305ca3
Meng Liu <meng.liu@agfa.com>
2021-01-22 15:21:19 +0800

    init

---
+++ pages/demo/demo.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<view>{{ username }}</view>
+		<view v-for="one in tel">{{ one }}</view>
+		<view v-if="age >= 18"><button @tap="signup()">我要报名</button></view>
+		<view><input type="text" v-model="address" placeholder="djs" /></view>
+		<view>{{ address }}</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			username: 'Fuck',
+			tel: ['23412423432', '34345344532'],
+			age: 17,
+			address: ''
+		};
+	},
+	methods: {
+		signup: function() {
+			uni.showToast({
+				title: '点击了报名按钮'
+			});
+		}
+	}
+};
+</script>
+
+<style></style>
